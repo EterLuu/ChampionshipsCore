b9c15568cafad8c31e7f6654314e93053af44e19
Eter Lu <eter.luu@ziip.ink>
2024-01-24 19:03:53 +0800

    Added avoidance of buddy injuries

--- src/main/java/ink/ziip/championshipscore/listener/PlayerListener.java
+++ src/main/java/ink/ziip/championshipscore/listener/PlayerListener.java
@@ -3,10 +3,18 @@ package ink.ziip.championshipscore.listener;
 import ink.ziip.championshipscore.ChampionshipsCore;
 import ink.ziip.championshipscore.api.BaseListener;
 import ink.ziip.championshipscore.api.player.CCPlayerManager;
+import ink.ziip.championshipscore.api.team.Team;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.entity.ThrownPotion;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.projectiles.ProjectileSource;
 
 public class PlayerListener extends BaseListener {
     protected PlayerListener(ChampionshipsCore plugin) {
@@ -15,13 +23,46 @@ public class PlayerListener extends BaseListener {
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerQuit(PlayerQuitEvent event) {
-        CCPlayerManager CCPlayerManager = ChampionshipsCore.getInstance().getCcPlayerManager();
-        CCPlayerManager.getPlayer(event.getPlayer()).updatePlayer();
+        Player player = event.getPlayer();
+        CCPlayerManager ccPlayerManager = ChampionshipsCore.getInstance().getCcPlayerManager();
+        ccPlayerManager.getPlayer(player).updatePlayer();
     }
 
     @EventHandler(priority = EventPriority.LOWEST)
     public void onPlayerJoin(PlayerJoinEvent event) {
-        CCPlayerManager CCPlayerManager = ChampionshipsCore.getInstance().getCcPlayerManager();
-        CCPlayerManager.getPlayer(event.getPlayer()).updatePlayer();
+        Player player = event.getPlayer();
+        CCPlayerManager ccPlayerManager = ChampionshipsCore.getInstance().getCcPlayerManager();
+        ccPlayerManager.getPlayer(player).updatePlayer();
+    }
+
+    @EventHandler
+    public void onPlayerDamagePlayer(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player assailant && event.getEntity() instanceof Player player) {
+            Team assailantTeam = plugin.getTeamManager().getTeamByPlayer(assailant);
+            if (assailantTeam != null) {
+                if (assailantTeam.equals(plugin.getTeamManager().getTeamByPlayer(player))) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+
+        if (event.getDamager() instanceof Arrow || event.getDamager() instanceof ThrownPotion) {
+            Projectile projectile = (Projectile) event.getDamager();
+            ProjectileSource projectileSource = projectile.getShooter();
+            if (!(projectileSource instanceof Player assailant))
+                return;
+
+            Team assailantTeam = plugin.getTeamManager().getTeamByPlayer(assailant);
+            if (assailantTeam != null) {
+                if (assailantTeam.equals(plugin.getTeamManager().getTeamByPlayer((Player) event.getEntity()))) {
+                    event.setCancelled(true);
+                }
+            }
+        }
+    }
+
+    @EventHandler
+    public void onPlayerHealedBySplashPotion(EntityRegainHealthEvent event) {
+        // TODO
     }
 }
